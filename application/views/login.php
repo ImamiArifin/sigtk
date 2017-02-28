8cffa981c26b3aebed4ac77eebe1342b0e450d23
Imami Arifin <imam_9216@gmail.com>
2017-02-28 09:20:21 +0700

    Upload Pertama
    Upload SIGTK pertama kali

---
+++ application/views/login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sistem Informasi Gelar, Tanda Jasa, dan Tanda Kehormatan</title>
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <!-- Bootstrap 3.3.2 -->
+        <link href="<?php echo base_url('assets/AdminLTE-2.0.5/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet" type="text/css" />
+        <!-- Font Awesome Icons -->
+        <link href="<?php echo base_url('assets/font-awesome-4.3.0/css/font-awesome.min.css') ?>" rel="stylesheet" type="text/css" />
+        <!-- Theme style -->
+        <link href="<?php echo base_url('assets/AdminLTE-2.0.5/dist/css/AdminLTE.min.css') ?>" rel="stylesheet" type="text/css" />
+        <!-- iCheck -->
+        <link href="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/iCheck/square/blue.css') ?>" rel="stylesheet" type="text/css" />
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body style="background-color: white;">
+        <div class="login-box">
+            <div class="login-logo">
+                <img src="http://2.bp.blogspot.com/-5avhv25WH7Y/U_U88YypO_I/AAAAAAAAAcs/ODuMnRcokSU/s1600/cpns%2BKementerian%2BSekretariat%2BNegara%2BRepublik%2BIndonesia-Setneg%2BRI%2Btahun%2B2014.png"/>
+            </div><!-- /.login-logo -->
+            <div class="login-box-body">
+                <p class="login-box-msg"><strong>Masukkan Username &amp; Password</strong></p>
+                <form action="<?php echo site_url('auth/login') ?>" method="post">
+                    <div class="form-group has-feedback">
+                        <input type="text" class="form-control" placeholder="Username"/>
+                        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                    </div>
+                    <div class="form-group has-feedback">
+                        <input type="password" class="form-control" placeholder="Password"/>
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <button type="submit" class="btn btn-success btn-block btn-flat">Proses</button>
+                        </div><!-- /.col -->
+                        <div class="col-xs-6">
+                            <button type="reset" class="btn btn-danger btn-block btn-flat">Batal</button>
+                        </div><!-- /.col -->
+                    </div>
+                </form>
+            </div><!-- /.login-box-body -->
+        </div><!-- /.login-box -->
+
+        <!-- jQuery 2.1.3 -->
+        <script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/jQuery/jQuery-2.1.3.min.js') ?>"></script>
+        <!-- Bootstrap 3.3.2 JS -->
+        <script src="<?php echo base_url('assets/AdminLTE-2.0.5/bootstrap/js/bootstrap.min.js') ?>" type="text/javascript"></script>
+        <!-- iCheck -->
+        <script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/iCheck/icheck.min.js') ?>" type="text/javascript"></script>
+        <script>
+            $(function () {
+                $('input').iCheck({
+                    checkboxClass: 'icheckbox_square-blue',
+                    radioClass: 'iradio_square-blue',
+                    increaseArea: '20%' // optional
+                });
+            });
+        </script>
+    </body>
+</html>
---
+++ application/views/template/js.php
@@ -0,0 +1,24 @@
+</div><!-- /.content-wrapper -->
+
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0
+    </div>
+    <strong>Copyright &copy; <?php echo date("Y");?> <a href="#">SIGTK</a>.</strong> All rights reserved.
+</footer>
+</div><!-- ./wrapper -->
+
+<!-- jQuery 2.1.3 -->
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/jQuery/jQuery-2.1.3.min.js') ?>"></script>
+<!-- Bootstrap 3.3.2 JS -->
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/bootstrap/js/bootstrap.min.js') ?>" type="text/javascript"></script>
+<!-- SlimScroll -->
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/slimScroll/jquery.slimScroll.min.js') ?>" type="text/javascript"></script>
+
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/datatables/jquery.dataTables.js')?>"></script>
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/plugins/datatables/dataTables.bootstrap.js')?>"></script>
+
+<!-- FastClick -->
+<script src='<?php echo base_url('assets/AdminLTE-2.0.5/plugins/fastclick/fastclick.min.js') ?>'></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url('assets/AdminLTE-2.0.5/dist/js/app.min.js') ?>" type="text/javascript"></script>
